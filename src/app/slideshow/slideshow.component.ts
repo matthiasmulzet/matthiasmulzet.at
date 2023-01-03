ecfd3bb2bf932946040bded5d50761edb1b9db71
matthiasmulzet <mulzetmatthias@gmail.com>
2023-01-03 22:17:18 +0100

    first commit

---
+++ src/app/slideshow/slideshow.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slideshow',
+  templateUrl: './slideshow.component.html',
+  styleUrls: ['./slideshow.component.scss']
+})
+export class SlideshowComponent implements OnInit {
+  images = ['computer.jpg', 'keyboard.jpg', 'man.jpg']
+  headlines = ['Bring engineering to the next level',
+    'Born to code',
+    'Graduated at Developer Akademie'
+  ];
+  currentImage = 0;
+
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+      setTimeout(() => {
+        this.showImage = true;
+      }, 10);
+    }, 8000);
+  }
+}
